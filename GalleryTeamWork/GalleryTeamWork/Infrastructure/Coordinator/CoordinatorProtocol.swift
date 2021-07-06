082059cc5d5ab2a7401c53c3cfac71d2ce6de79f
AniMekvab <vottakhoney@gmail.com>
2021-07-07 01:06:07 +0400

    no message

--- GalleryTeamWork/GalleryTeamWork/Infrastructure/Coordinator/CoordinatorProtocol.swift
+++ GalleryTeamWork/GalleryTeamWork/Infrastructure/Coordinator/CoordinatorProtocol.swift
@@ -7,6 +7,8 @@ protocol CoordinatorProtocol: AnyObject {
     init(_ window: UIWindow?, navigationController: UINavigationController?)
     
     func start()
+    func popViewController()
+    func proceedToFavouritesVC(coordinator: CoordinatorProtocol)
     func proceedToEdit(with image: UIImage)
     func alertProblem()
 
